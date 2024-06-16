a9e266f83041424192aebe20bfdb37d60a2380f7
aboutamirhamza <amirhamzashuvokhan@gmail.com>
2024-06-16 16:15:32 +0600

    Airfolio Responsive HTML CSS JS Complete Project

---
+++ js/script.js
@@ -0,0 +1,47 @@
+// For Testimonial Slider JS Code Start Here
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("testimonial_slide");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+// For Testimonial Slider JS Code End Here
+
+// For Header Menu JS Code Start Here
+let menuList = document.getElementById("menuList");
+menuList.style.maxHeight = "0px";
+
+function toggleMenu() {
+  if (menuList.style.maxHeight == "0px") {
+    menuList.style.maxHeight = "350px";
+  } else {
+    menuList.style.maxHeight = "0px";
+  }
+}
+// For Header Menu JS Code End Here
